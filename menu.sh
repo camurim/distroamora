5b6d5403448ca3e64467c25d24494d40832aeced
Carlos Amorim <carlos.amorim@sps.ce.gov.br>
2024-12-03 15:22:19 -0300

    new functions

--- menu.sh
+++ menu.sh
@@ -32,9 +32,29 @@ function show_progress() {
 	# output the bar
 	echo -ne "\rProgress : [${done_sub_bar}${todo_sub_bar}] ${percent}%"
 
-	if [ $total -eq $current ]; then
+	if [ "$total" -eq "$current" ]; then
 		echo -e "\nDONE"
 	fi
+}
+
+##------------------------------------------------------------------------------------
+## Configuração inicial de diretórios
+##
+
+function configUserDirectories() {
+	if [ ! -d "$HOME"/google-drive ]; then
+		return 1
+	fi
+
+	[[ ! -d "$HOME"/_junkdrawer ]] && mkdir -p "$HOME"/_junkdrawer
+
+	# Diretórios do Google Driver
+	[[ ! -d "$HOME"/Documentos/docs ]] && ln -s "$HOME"/google-drive/docs "$HOME"/Documentos/docs
+	[[ ! -d "$HOME"/Documentos/docs-archive ]] && ln -s "$HOME"/google-drive/docs-archive "$HOME"/Documentos/docs
+	[[ ! -d "$HOME"/Documentos/LTS ]] && ln -s "$HOME"/google-drive/LTS "$HOME"/Documentos/docs
+	[[ ! -d "$HOME"/Documentos/projects ]] && ln -s "$HOME"/google-drive/projects "$HOME"/Documentos/docs
+	[[ ! -d "$HOME"/keyring ]] && ln -s "$HOME"/google-drive/keyring "$HOME"/keyring
+	[[ ! -d "$HOME"/scripts ]] && ln -s "$HOME"/google-drive/scripts "$HOME"/scripts
 }
 
 ##------------------------------------------------------------------------------------
@@ -42,13 +62,101 @@ function show_progress() {
 ##
 
 function installPrerequisites() {
-	[[ $(dpkg -s whiptail >/dev/null 2>&1) -ne 0 ]] && apt-get install whiptail -y
-	[[ $(dpkg -s gawk >/dev/null 2>&1) -ne 0 ]] && apt-get install gawk -y
-	[[ $(dpkg -s libreoffice >/dev/null 2>&1) -ne 0 ]] && apt-get install libreoffice --no-install-recommends -y
+	sudo apt-get update -y
+	sudo apt-get upgrade -y
+	[[ $(dpkg -s whiptail >/dev/null 2>&1) -ne 0 ]] && sudo apt-get install whiptail -y
+	[[ $(dpkg -s wget >/dev/null 2>&1) -ne 0 ]] && sudo apt-get install wget -y
+	[[ $(dpkg -s curl >/dev/null 2>&1) -ne 0 ]] && sudo apt-get install curl -y
+	[[ $(dpkg -s rclone >/dev/null 2>&1) -ne 0 ]] && sudo apt-get install rclone -y
+	[[ $(dpkg -s gawk >/dev/null 2>&1) -ne 0 ]] && sudo apt-get install gawk -y
+	[[ $(dpkg -s libreoffice >/dev/null 2>&1) -ne 0 ]] && sudo apt-get install libreoffice --no-install-recommends -y
 }
 
 ##--------------------------------------------------------------------------------------
-## Instalar pré-requisitos
+## Instalar ferramentas de desenvolvimento
+##
+
+function installdevTools() {
+	[[ $(dpkg -s build-essential >/dev/null 2>&1) -ne 0 ]] && sudo apt-get install build-essential -y
+	[[ $(dpkg -s fakeroot >/dev/null 2>&1) -ne 0 ]] && sudo apt-get install fakeroot -y
+	[[ $(dpkg -s devscripts >/dev/null 2>&1) -ne 0 ]] && sudo apt-get install devscripts -y
+	[[ $(dpkg -s make >/dev/null 2>&1) -ne 0 ]] && sudo apt-get install make -y
+	[[ $(dpkg -s cmake >/dev/null 2>&1) -ne 0 ]] && sudo apt-get install cmake -y
+	[[ $(dpkg -s ninja-build >/dev/null 2>&1) -ne 0 ]] && sudo apt-get install ninja-build -y
+	[[ $(dpkg -s meson >/dev/null 2>&1) -ne 0 ]] && sudo apt-get install meson -y
+	[[ $(dpkg -s linux-headers-"$(uname -r)" >/dev/null 2>&1) -ne 0 ]] && sudo apt-get install linux-headers-"$(uname -r)" -y
+	[[ $(dpkg -s libglvnd-dev >/dev/null 2>&1) -ne 0 ]] && sudo apt-get install libglvnd-dev -y
+	[[ $(dpkg -s pkg-config >/dev/null 2>&1) -ne 0 ]] && sudo apt-get install pkg-config -y
+	[[ $(dpkg -s vim >/dev/null 2>&1) -ne 0 ]] && sudo apt-get install vim -y
+	[[ $(dpkg -s neovim >/dev/null 2>&1) -ne 0 ]] && sudo apt-get install neovim -y
+	[[ $(dpkg -s git >/dev/null 2>&1) -ne 0 ]] && sudo apt-get install git -y
+	[[ $(dpkg -s gh >/dev/null 2>&1) -ne 0 ]] && sudo apt-get install gh -y
+	[[ $(dpkg -s python3-all >/dev/null 2>&1) -ne 0 ]] && sudo apt-get install python3-all -y
+	python3 -m ensurepip --upgrade
+	curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+}
+
+##--------------------------------------------------------------------------------------
+## Instalar utilitários
+##
+
+function installUtils() {
+	[[ $(dpkg -s keepassxc >/dev/null 2>&1) -ne 0 ]] && sudo apt-get install keepassxc -y
+	[[ $(dpkg -s inotify-tools >/dev/null 2>&1) -ne 0 ]] && sudo apt-get install inotify-tools -y
+	[[ $(dpkg -s lolcat >/dev/null 2>&1) -ne 0 ]] && sudo apt-get install lolcat -y
+	[[ $(dpkg -s kitty >/dev/null 2>&1) -ne 0 ]] && sudo apt-get install kitty -y
+	[[ $(dpkg -s zenity >/dev/null 2>&1) -ne 0 ]] && sudo apt-get install zenity -y
+	[[ $(dpkg -s scrot >/dev/null 2>&1) -ne 0 ]] && sudo apt-get install scrot -y
+	[[ $(dpkg -s flameshot >/dev/null 2>&1) -ne 0 ]] && sudo apt-get install flameshot -y
+	[[ $(dpkg -s gromit >/dev/null 2>&1) -ne 0 ]] && sudo apt-get install gromit -y
+	[[ $(dpkg -s gromit-mpx >/dev/null 2>&1) -ne 0 ]] && sudo apt-get install gromit-mpx -y
+	[[ $(dpkg -s feh >/dev/null 2>&1) -ne 0 ]] && sudo apt-get install feh -y
+	[[ $(dpkg -s magnus >/dev/null 2>&1) -ne 0 ]] && sudo apt-get install magnus -y
+	[[ $(dpkg -s qalculate-gtk >/dev/null 2>&1) -ne 0 ]] && sudo apt-get install qalculate-gtk -y
+	[[ $(dpkg -s mpv >/dev/null 2>&1) -ne 0 ]] && sudo apt-get install mpv -y
+	[[ $(dpkg -s vlc >/dev/null 2>&1) -ne 0 ]] && sudo apt-get install vlc -y
+	[[ $(dpkg -s cmus >/dev/null 2>&1) -ne 0 ]] && sudo apt-get install cmus -y
+	[[ $(dpkg -s deadbeef >/dev/null 2>&1) -ne 0 ]] && sudo apt-get install deadbeef -y
+	[[ $(dpkg -s exa >/dev/null 2>&1) -ne 0 ]] && sudo apt-get install exa -y
+	[[ $(dpkg -s netcat >/dev/null 2>&1) -ne 0 ]] && sudo apt-get install netcat -y
+	[[ $(dpkg -s netcat-traditional >/dev/null 2>&1) -ne 0 ]] && sudo apt-get install netcat-traditional -y
+	[[ $(dpkg -s prettyping >/dev/null 2>&1) -ne 0 ]] && sudo apt-get install prettyping -y
+	[[ $(dpkg -s nala >/dev/null 2>&1) -ne 0 ]] && sudo apt-get install nala -y
+	[[ $(dpkg -s htop >/dev/null 2>&1) -ne 0 ]] && sudo apt-get install htop -y
+	[[ $(dpkg -s btop >/dev/null 2>&1) -ne 0 ]] && sudo apt-get install btop -y
+	[[ $(dpkg -s rofi >/dev/null 2>&1) -ne 0 ]] && sudo apt-get install rofi -y
+	[[ $(dpkg -s newsboat >/dev/null 2>&1) -ne 0 ]] && sudo apt-get install newsboat -y
+	[[ $(dpkg -s pulsemixer >/dev/null 2>&1) -ne 0 ]] && sudo apt-get install pulsemixer -y
+	[[ $(dpkg -s neofetch >/dev/null 2>&1) -ne 0 ]] && sudo apt-get install neofetch -y
+	[[ $(dpkg -s screenfetch >/dev/null 2>&1) -ne 0 ]] && sudo apt-get install screenfetch -y
+	[[ $(dpkg -s mp3info >/dev/null 2>&1) -ne 0 ]] && sudo apt-get install mp3info -y
+	[[ $(dpkg -s trash-cli >/dev/null 2>&1) -ne 0 ]] && sudo apt-get install trash-cli -y
+	[[ $(dpkg -s calcurse >/dev/null 2>&1) -ne 0 ]] && sudo apt-get install calcurse -y
+	[[ $(dpkg -s fzf >/dev/null 2>&1) -ne 0 ]] && sudo apt-get install fzf -y
+	[[ $(dpkg -s filezilla >/dev/null 2>&1) -ne 0 ]] && sudo apt-get install filezilla -y
+}
+
+##--------------------------------------------------------------------------------------
+## Instalar QTile
+##
+
+function installQtile() {
+	sudo apt install libconfig-dev libdbus-1-dev libegl-dev libev-dev libgl-dev libepoxy-dev libpcre2-dev libpixman-1-dev libx11-xcb-dev libxcb1-dev libxcb-composite0-dev libxcb-damage0-dev libxcb-glx0-dev libxcb-image0-dev libxcb-present-dev libxcb-randr0-dev libxcb-render0-dev libxcb-render-util0-dev libxcb-shape0-dev libxcb-util-dev libxcb-xfixes0-dev meson ninja-build uthash-dev -y
+	[[ ! -d "$HOME"/src ]] && mkdir "$HOME"/src
+	git clone https://github.com/yshui/picom.git "$HOME"/src
+	cd "$HOME"/src/picom || return
+	meson setup --buildtype=release build
+	ninja -C build
+	sudo cp build/src/picom /usr/local/bin
+}
+
+##--------------------------------------------------------------------------------------
+## Instalação
 ##
 
 installPrerequisites
+installdevTools
+
+##--------------------------------------------------------------------------------------
+##
+##
