7b925c57bd28fce9ee26ca9bb3981f3aaa19b722
caurim <caamorimcoelho@gmail.com>
2024-12-14 12:18:17 -0300

    new functions added

--- menu.sh
+++ menu.sh
@@ -45,16 +45,16 @@ function installPrerequisites() {
 	fi
 	sudo apt-get update -y
 	sudo apt-get upgrade -y
-	[[ $(dpkg -s whiptail >/dev/null 2>&1) -ne 0 ]] && sudo apt-get install whiptail -y
-	[[ $(dpkg -s wget >/dev/null 2>&1) -ne 0 ]] && sudo apt-get install wget -y
-	[[ $(dpkg -s curl >/dev/null 2>&1) -ne 0 ]] && sudo apt-get install curl -y
-	[[ $(dpkg -s rclone >/dev/null 2>&1) -ne 0 ]] && sudo apt-get install rclone -y
-	[[ $(dpkg -s gawk >/dev/null 2>&1) -ne 0 ]] && sudo apt-get install gawk -y
-	[[ $(dpkg -s python3-all >/dev/null 2>&1) -ne 0 ]] && sudo apt-get install python3-all -y
-	[[ $(dpkg -s temurin-8-jdk >/dev/null 2>&1) -ne 0 ]] && sudo apt-get install temurin-8-jdk -y
-	[[ $(dpkg -s temurin-17-jdk >/dev/null 2>&1) -ne 0 ]] && sudo apt-get install temurin-17-jdk -y
-	[[ $(dpkg -s temurin-23-jdk >/dev/null 2>&1) -ne 0 ]] && sudo apt-get install temurin-23-jdk -y
-	[[ $(dpkg -s libreoffice >/dev/null 2>&1) -ne 0 ]] && sudo apt-get install libreoffice --no-install-recommends -y
+	! dpkg -s whiptail >/dev/null 2>&1 && sudo apt-get install whiptail -y
+	! dpkg -s wget >/dev/null 2>&1 && sudo apt-get install wget -y
+	! dpkg -s curl >/dev/null 2>&1 && sudo apt-get install curl -y
+	! dpkg -s rclone >/dev/null 2>&1 && sudo apt-get install rclone -y
+	! dpkg -s gawk >/dev/null 2>&1 && sudo apt-get install gawk -y
+	! dpkg -s python3-all >/dev/null 2>&1 && sudo apt-get install python3-all -y
+	! dpkg -s temurin-8-jdk >/dev/null 2>&1 && sudo apt-get install temurin-8-jdk -y
+	! dpkg -s temurin-17-jdk >/dev/null 2>&1 && sudo apt-get install temurin-17-jdk -y
+	! dpkg -s temurin-23-jdk >/dev/null 2>&1 && sudo apt-get install temurin-23-jdk -y
+	! dpkg -s libreoffice >/dev/null 2>&1 && sudo apt-get install libreoffice --no-install-recommends -y
 	sudo apt-get install cabextract libmspack0 ttf-mscorefonts-installer -y
 }
 
@@ -80,8 +80,8 @@ function installLibraries() {
 ##
 
 function installPlFonts() {
-	[[ $(dpkg -s powerline >/dev/null 2>&1) -ne 0 ]] && sudo apt-get install powerline -y
-	[[ $(dpkg -s fonts-powerline >/dev/null 2>&1) -ne 0 ]] && sudo apt-get install fonts-powerline -y
+	! dpkg -s powerline >/dev/null 2>&1 && sudo apt-get install powerline -y
+	! dpkg -s fonts-powerline >/dev/null 2>&1 && sudo apt-get install fonts-powerline -y
 }
 
 ##--------------------------------------------------------------------------------------
@@ -89,23 +89,23 @@ function installPlFonts() {
 ##
 
 function installDevTools() {
-	[[ $(dpkg -s build-essential >/dev/null 2>&1) -ne 0 ]] && sudo apt-get install build-essential -y
-	[[ $(dpkg -s fakeroot >/dev/null 2>&1) -ne 0 ]] && sudo apt-get install fakeroot -y
-	[[ $(dpkg -s devscripts >/dev/null 2>&1) -ne 0 ]] && sudo apt-get install devscripts -y
-	[[ $(dpkg -s make >/dev/null 2>&1) -ne 0 ]] && sudo apt-get install make -y
-	[[ $(dpkg -s cmake >/dev/null 2>&1) -ne 0 ]] && sudo apt-get install cmake -y
-	[[ $(dpkg -s ninja-build >/dev/null 2>&1) -ne 0 ]] && sudo apt-get install ninja-build -y
-	[[ $(dpkg -s meson >/dev/null 2>&1) -ne 0 ]] && sudo apt-get install meson -y
-	[[ $(dpkg -s linux-headers-"$(uname -r)" >/dev/null 2>&1) -ne 0 ]] && sudo apt-get install linux-headers-"$(uname -r)" -y
-	[[ $(dpkg -s libglvnd-dev >/dev/null 2>&1) -ne 0 ]] && sudo apt-get install libglvnd-dev -y
-	[[ $(dpkg -s pkg-config >/dev/null 2>&1) -ne 0 ]] && sudo apt-get install pkg-config -y
-	[[ $(dpkg -s vim >/dev/null 2>&1) -ne 0 ]] && sudo apt-get install vim -y
-	[[ $(dpkg -s neovim >/dev/null 2>&1) -ne 0 ]] && sudo apt-get install neovim -y
-	[[ $(dpkg -s git >/dev/null 2>&1) -ne 0 ]] && sudo apt-get install git -y
-	[[ $(dpkg -s gh >/dev/null 2>&1) -ne 0 ]] && sudo apt-get install gh -y
-	[[ $(dpkg -s binwalk >/dev/null 2>&1) -ne 0 ]] && sudo apt-get install binwalk -y
-	[[ $(dpkg -s python3-all >/dev/null 2>&1) -ne 0 ]] && sudo apt-get install python3-all -y
-	[[ $(dpkg -s python3-pip >/dev/null 2>&1) -ne 0 ]] && sudo apt-get install python3-pip -y
+	! dpkg -s build-essential >/dev/null 2>&1 && sudo apt-get install build-essential -y
+	! dpkg -s fakeroot >/dev/null 2>&1 && sudo apt-get install fakeroot -y
+	! dpkg -s devscripts >/dev/null 2>&1 && sudo apt-get install devscripts -y
+	! dpkg -s make >/dev/null 2>&1 && sudo apt-get install make -y
+	! dpkg -s cmake >/dev/null 2>&1 && sudo apt-get install cmake -y
+	! dpkg -s ninja-build >/dev/null 2>&1 && sudo apt-get install ninja-build -y
+	! dpkg -s meson >/dev/null 2>&1 && sudo apt-get install meson -y
+	! dpkg -s linux-headers-"$(uname -r)" >/dev/null 2>&1 && sudo apt-get install linux-headers-"$(uname -r)" -y
+	! dpkg -s libglvnd-dev >/dev/null 2>&1 && sudo apt-get install libglvnd-dev -y
+	! dpkg -s pkg-config >/dev/null 2>&1 && sudo apt-get install pkg-config -y
+	! dpkg -s vim >/dev/null 2>&1 && sudo apt-get install vim -y
+	! dpkg -s neovim >/dev/null 2>&1 && sudo apt-get install neovim -y
+	! dpkg -s git >/dev/null 2>&1 && sudo apt-get install git -y
+	! dpkg -s gh >/dev/null 2>&1 && sudo apt-get install gh -y
+	! dpkg -s binwalk >/dev/null 2>&1 && sudo apt-get install binwalk -y
+	! dpkg -s python3-all >/dev/null 2>&1 && sudo apt-get install python3-all -y
+	! dpkg -s python3-pip >/dev/null 2>&1 && sudo apt-get install python3-pip -y
 	
 	if [ ! -f "$HOME"/.cargo/bin/cargo ]; then
 		curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
@@ -117,55 +117,55 @@ function installDevTools() {
 ##
 
 function installUtilities() {
-	[[ $(dpkg -s alacritty >/dev/null 2>&1) -ne 0 ]] && sudo apt-get install alacritty -y
-	[[ $(dpkg -s kitty >/dev/null 2>&1) -ne 0 ]] && sudo apt-get install kitty -y
-	[[ $(dpkg -s keepassxc >/dev/null 2>&1) -ne 0 ]] && sudo apt-get install keepassxc -y
-	[[ $(dpkg -s inotify-tools >/dev/null 2>&1) -ne 0 ]] && sudo apt-get install inotify-tools -y
-	[[ $(dpkg -s lolcat >/dev/null 2>&1) -ne 0 ]] && sudo apt-get install lolcat -y
-	[[ $(dpkg -s zenity >/dev/null 2>&1) -ne 0 ]] && sudo apt-get install zenity -y
-	[[ $(dpkg -s scrot >/dev/null 2>&1) -ne 0 ]] && sudo apt-get install scrot -y
-	[[ $(dpkg -s flameshot >/dev/null 2>&1) -ne 0 ]] && sudo apt-get install flameshot -y
-	[[ $(dpkg -s gromit >/dev/null 2>&1) -ne 0 ]] && sudo apt-get install gromit -y
-	[[ $(dpkg -s gromit-mpx >/dev/null 2>&1) -ne 0 ]] && sudo apt-get install gromit-mpx -y
-	[[ $(dpkg -s feh >/dev/null 2>&1) -ne 0 ]] && sudo apt-get install feh -y
-	[[ $(dpkg -s magnus >/dev/null 2>&1) -ne 0 ]] && sudo apt-get install magnus -y
-	[[ $(dpkg -s qalculate-gtk >/dev/null 2>&1) -ne 0 ]] && sudo apt-get install qalculate-gtk -y
-	[[ $(dpkg -s mpv >/dev/null 2>&1) -ne 0 ]] && sudo apt-get install mpv -y
-	[[ $(dpkg -s vlc >/dev/null 2>&1) -ne 0 ]] && sudo apt-get install vlc -y
-	[[ $(dpkg -s cmus >/dev/null 2>&1) -ne 0 ]] && sudo apt-get install cmus -y
-	[[ $(dpkg -s deadbeef >/dev/null 2>&1) -ne 0 ]] && sudo apt-get install deadbeef -y
-	[[ $(dpkg -s exa >/dev/null 2>&1) -ne 0 ]] && sudo apt-get install exa -y
-	[[ $(dpkg -s netcat >/dev/null 2>&1) -ne 0 ]] && sudo apt-get install netcat -y
-	[[ $(dpkg -s netcat-traditional >/dev/null 2>&1) -ne 0 ]] && sudo apt-get install netcat-traditional -y
-	[[ $(dpkg -s prettyping >/dev/null 2>&1) -ne 0 ]] && sudo apt-get install prettyping -y
-	[[ $(dpkg -s nala >/dev/null 2>&1) -ne 0 ]] && sudo apt-get install nala -y
-	[[ $(dpkg -s htop >/dev/null 2>&1) -ne 0 ]] && sudo apt-get install htop -y
-	[[ $(dpkg -s rofi >/dev/null 2>&1) -ne 0 ]] && sudo apt-get install rofi -y
-	[[ $(dpkg -s newsboat >/dev/null 2>&1) -ne 0 ]] && sudo apt-get install newsboat -y
-	[[ $(dpkg -s pulsemixer >/dev/null 2>&1) -ne 0 ]] && sudo apt-get install pulsemixer -y
-	[[ $(dpkg -s neofetch >/dev/null 2>&1) -ne 0 ]] && sudo apt-get install neofetch -y
-	[[ $(dpkg -s screenfetch >/dev/null 2>&1) -ne 0 ]] && sudo apt-get install screenfetch -y
-	[[ $(dpkg -s mp3info >/dev/null 2>&1) -ne 0 ]] && sudo apt-get install mp3info -y
-	[[ $(dpkg -s trash-cli >/dev/null 2>&1) -ne 0 ]] && sudo apt-get install trash-cli -y
-	[[ $(dpkg -s calcurse >/dev/null 2>&1) -ne 0 ]] && sudo apt-get install calcurse -y
-	[[ $(dpkg -s fzf >/dev/null 2>&1) -ne 0 ]] && sudo apt-get install fzf -y
-	[[ $(dpkg -s fd-find >/dev/null 2>&1) -ne 0 ]] && sudo apt-get install fd-find -y
-	[[ $(dpkg -s f3 >/dev/null 2>&1) -ne 0 ]] && sudo apt-get install f3 -y
-	[[ $(dpkg -s ripgrep >/dev/null 2>&1) -ne 0 ]] && sudo apt-get install ripgrep -y
-	[[ $(dpkg -s duf >/dev/null 2>&1) -ne 0 ]] && sudo apt-get install duf -y
-	[[ $(dpkg -s unclutter >/dev/null 2>&1) -ne 0 ]] && sudo apt-get install unclutter -y
-	[[ $(dpkg -s numlockx >/dev/null 2>&1) -ne 0 ]] && sudo apt-get install numlockx -y
-	[[ $(dpkg -s xss-lock >/dev/null 2>&1) -ne 0 ]] && sudo apt-get install xss-lock -y
-	[[ $(dpkg -s xclip >/dev/null 2>&1) -ne 0 ]] && sudo apt-get install xclip -y
-	[[ $(dpkg -s x11-apps >/dev/null 2>&1) -ne 0 ]] && sudo apt-get install x11-apps -y
-	[[ $(dpkg -s filezilla >/dev/null 2>&1) -ne 0 ]] && sudo apt-get install filezilla -y
-	[[ $(dpkg -s kcolorchooser >/dev/null 2>&1) -ne 0 ]] && sudo apt-get install kcolorchooser -y
-	[[ $(dpkg -s kronometer >/dev/null 2>&1) -ne 0 ]] && sudo apt-get install kronometer -y
-	[[ $(dpkg -s rar >/dev/null 2>&1) -ne 0 ]] && sudo apt-get install rar -y
-	[[ $(dpkg -s unrar >/dev/null 2>&1) -ne 0 ]] && sudo apt-get install unrar -y
-	[[ $(dpkg -s nmap >/dev/null 2>&1) -ne 0 ]] && sudo apt-get install nmap -y
-	[[ $(dpkg -s obs-studio >/dev/null 2>&1) -ne 0 ]] && sudo apt-get install obs-studio -y
-	[[ $(dpkg -s psensor >/dev/null 2>&1) -ne 0 ]] && sudo apt-get install psensor -y
+	! dpkg -s alacritty >/dev/null 2>&1 && sudo apt-get install alacritty -y
+	! dpkg -s kitty >/dev/null 2>&1 && sudo apt-get install kitty -y
+	! dpkg -s keepassxc >/dev/null 2>&1 && sudo apt-get install keepassxc -y
+	! dpkg -s inotify-tools >/dev/null 2>&1 && sudo apt-get install inotify-tools -y
+	! dpkg -s lolcat >/dev/null 2>&1 && sudo apt-get install lolcat -y
+	! dpkg -s zenity >/dev/null 2>&1 && sudo apt-get install zenity -y
+	! dpkg -s scrot >/dev/null 2>&1 && sudo apt-get install scrot -y
+	! dpkg -s flameshot >/dev/null 2>&1 && sudo apt-get install flameshot -y
+	! dpkg -s gromit >/dev/null 2>&1 && sudo apt-get install gromit -y
+	! dpkg -s gromit-mpx >/dev/null 2>&1 && sudo apt-get install gromit-mpx -y
+	! dpkg -s feh >/dev/null 2>&1 && sudo apt-get install feh -y
+	! dpkg -s magnus >/dev/null 2>&1 && sudo apt-get install magnus -y
+	! dpkg -s qalculate-gtk >/dev/null 2>&1 && sudo apt-get install qalculate-gtk -y
+	! dpkg -s mpv >/dev/null 2>&1 && sudo apt-get install mpv -y
+	! dpkg -s vlc >/dev/null 2>&1 && sudo apt-get install vlc -y
+	! dpkg -s cmus >/dev/null 2>&1 && sudo apt-get install cmus -y
+	! dpkg -s deadbeef >/dev/null 2>&1 && sudo apt-get install deadbeef -y
+	! dpkg -s exa >/dev/null 2>&1 && sudo apt-get install exa -y
+	! dpkg -s netcat >/dev/null 2>&1 && sudo apt-get install netcat -y
+	! dpkg -s netcat-traditional >/dev/null 2>&1 && sudo apt-get install netcat-traditional -y
+	! dpkg -s prettyping >/dev/null 2>&1 && sudo apt-get install prettyping -y
+	! dpkg -s nala >/dev/null 2>&1 && sudo apt-get install nala -y
+	! dpkg -s htop >/dev/null 2>&1 && sudo apt-get install htop -y
+	! dpkg -s rofi >/dev/null 2>&1 && sudo apt-get install rofi -y
+	! dpkg -s newsboat >/dev/null 2>&1 && sudo apt-get install newsboat -y
+	! dpkg -s pulsemixer >/dev/null 2>&1 && sudo apt-get install pulsemixer -y
+	! dpkg -s neofetch >/dev/null 2>&1 && sudo apt-get install neofetch -y
+	! dpkg -s screenfetch >/dev/null 2>&1 && sudo apt-get install screenfetch -y
+	! dpkg -s mp3info >/dev/null 2>&1 && sudo apt-get install mp3info -y
+	! dpkg -s trash-cli >/dev/null 2>&1 && sudo apt-get install trash-cli -y
+	! dpkg -s calcurse >/dev/null 2>&1 && sudo apt-get install calcurse -y
+	! dpkg -s fzf >/dev/null 2>&1 && sudo apt-get install fzf -y
+	! dpkg -s fd-find >/dev/null 2>&1 && sudo apt-get install fd-find -y
+	! dpkg -s f3 >/dev/null 2>&1 && sudo apt-get install f3 -y
+	! dpkg -s ripgrep >/dev/null 2>&1 && sudo apt-get install ripgrep -y
+	! dpkg -s duf >/dev/null 2>&1 && sudo apt-get install duf -y
+	! dpkg -s unclutter >/dev/null 2>&1 && sudo apt-get install unclutter -y
+	! dpkg -s numlockx >/dev/null 2>&1 && sudo apt-get install numlockx -y
+	! dpkg -s xss-lock >/dev/null 2>&1 && sudo apt-get install xss-lock -y
+	! dpkg -s xclip >/dev/null 2>&1 && sudo apt-get install xclip -y
+	! dpkg -s x11-apps >/dev/null 2>&1 && sudo apt-get install x11-apps -y
+	! dpkg -s filezilla >/dev/null 2>&1 && sudo apt-get install filezilla -y
+	! dpkg -s kcolorchooser >/dev/null 2>&1 && sudo apt-get install kcolorchooser -y
+	! dpkg -s kronometer >/dev/null 2>&1 && sudo apt-get install kronometer -y
+	! dpkg -s rar >/dev/null 2>&1 && sudo apt-get install rar -y
+	! dpkg -s unrar >/dev/null 2>&1 && sudo apt-get install unrar -y
+	! dpkg -s nmap >/dev/null 2>&1 && sudo apt-get install nmap -y
+	! dpkg -s obs-studio >/dev/null 2>&1 && sudo apt-get install obs-studio -y
+	! dpkg -s psensor >/dev/null 2>&1 && sudo apt-get install psensor -y
 }
 
 ##--------------------------------------------------------------------------------------
@@ -173,10 +173,10 @@ function installUtilities() {
 ##
 
 function installAccessories() {
-	[[ $(dpkg -s gnome-screensaver >/dev/null 2>&1) -ne 0 ]] && sudo apt-get install gnome-screensaver -y
-	[[ $(dpkg -s gnome-bluetooth >/dev/null 2>&1) -ne 0 ]] && sudo apt-get install gnome-bluetooth -y
-	[[ $(dpkg -s cava >/dev/null 2>&1) -ne 0 ]] && sudo apt-get install cava -y
-	[[ $(dpkg -s screenkey >/dev/null 2>&1) -ne 0 ]] && sudo apt-get install screenkey -y
+	! dpkg -s gnome-screensaver >/dev/null 2>&1 && sudo apt-get install gnome-screensaver -y
+	! dpkg -s gnome-bluetooth >/dev/null 2>&1 && sudo apt-get install gnome-bluetooth -y
+	! dpkg -s cava >/dev/null 2>&1 && sudo apt-get install cava -y
+	! dpkg -s screenkey >/dev/null 2>&1 && sudo apt-get install screenkey -y
 }
 
 ##--------------------------------------------------------------------------------------
@@ -184,10 +184,10 @@ function installAccessories() {
 ##
 
 function installTechSciSoftware() {
-	[[ $(dpkg -s kikad >/dev/null 2>&1) -ne 0 ]] && sudo apt-get install kicad -y
-	[[ $(dpkg -s logisim >/dev/null 2>&1) -ne 0 ]] && sudo apt-get install logisim -y
-	[[ $(dpkg -s simulide >/dev/null 2>&1) -ne 0 ]] && sudo apt-get install simulide -y
-	[[ $(dpkg -s glogic >/dev/null 2>&1) -ne 0 ]] && sudo apt-get install glogic -y
+	! dpkg -s kikad >/dev/null 2>&1 && sudo apt-get install kicad -y
+	! dpkg -s logisim >/dev/null 2>&1 && sudo apt-get install logisim -y
+	! dpkg -s simulide >/dev/null 2>&1 && sudo apt-get install simulide -y
+	! dpkg -s glogic >/dev/null 2>&1 && sudo apt-get install glogic -y
 }
 
 ##--------------------------------------------------------------------------------------
@@ -228,7 +228,7 @@ function installQtile() {
 ##
 
 function installNvidiaDriver() {
-	[[ $(dpkg -s nvidia-driver >/dev/null 2>&1) -ne 0 ]] && sudo apt-get install nvidia-driver -y
+	! dpkg -s nvidia-driver >/dev/null 2>&1 && sudo apt-get install nvidia-driver -y
 }
 
 
